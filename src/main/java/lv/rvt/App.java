74998f14477c3466a3f60d98a5b506926f8561ee
23DP1ALips <166118019+23DP1ALips@users.noreply.github.com>
2024-09-10 09:15:14 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,6 +2,7 @@ package lv.rvt;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.util.Scanner;
 
 import javax.swing.plaf.TreeUI;
 
@@ -10,13 +11,21 @@ public class App
     public static void main( String[] args )
     {
         
-        String name = "Alberts";
-        int age = 16;
-        boolean isOnline = true;
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter you're name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter you're lastname: ");
+        String lastName = scanner.nextLine();
+        System.out.println("Enter you're age: ");
+        String age = scanner.nextLine();
+        System.out.println("Enter you're group: ");
+        String group = scanner.nextLine();
 
-        System.out.println(name);
-        System.out.println(age);
-        System.out.println(isOnline);
+        String start = ("You're name is " + name + " " + lastName);
+        String mid = (". You are " + age + " years old. ");
+        String end = ("You're group is " + group + ".");
+        System.out.println(start + mid + end);
 
     }
 
