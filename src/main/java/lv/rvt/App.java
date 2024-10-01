62b78e2b87da2b4ffa97c76f1bd8e1a22c194e13
23DP1ALips <166118019+23DP1ALips@users.noreply.github.com>
2024-10-01 06:13:51 +0000

    loop

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,22 +8,13 @@ public class App
     {
         
         Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Value of the gift?");
-        int valueOfTheGift = Integer.valueOf(scanner.nextLine());
+        int start = 3;
+        int end = 7;
+        int i = start;
+        while (i < end) {
+            System.out.println(i);
+            i++;
+        }        
 
-        if (valueOfTheGift < 5000) {
-            System.out.println("No tax!");
-        } else if (valueOfTheGift < 25000) {
-            System.out.println("Tax: " + (100 + (valueOfTheGift-5000) * 0.08));
-        } else if (valueOfTheGift < 55000) {
-            System.out.println("Tax: " + (1700 + (valueOfTheGift-25000) * 0.1));
-        } else if (valueOfTheGift < 200000) {
-            System.out.println("Tax: " + (4700 + (valueOfTheGift-55000) * 0.12));
-        } else if (valueOfTheGift < 1000000) {
-            System.out.println("Tax: " + (22100 + (valueOfTheGift-200000) * 0.15));
-        } else {
-            System.out.println("Tax: " + (142100 + (valueOfTheGift-1000000) * 0.17));
-        }
     }
 }
