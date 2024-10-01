1768e36188ba73a66564223af7e5fec4ad7809f3
23DP1ALips <166118019+23DP1ALips@users.noreply.github.com>
2024-10-01 06:36:08 +0000

    even and odd + other

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,13 +8,37 @@ public class App
     {
         
         Scanner scanner = new Scanner(System.in);
-        int start = 3;
-        int end = 7;
-        int i = start;
-        while (i < end) {
-            System.out.println(i);
-            i++;
-        }        
+        System.out.println("Enter numbers:");
+        int sum = 0;
+        int numb = 0;
+        int even = 0;
+        int odd = 0;
 
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+
+            if (input < 0) {
+                System.out.println("Thx! Bye!");
+                break;
+            }
+
+            if (input >= 0) {
+                sum += input;
+                numb += 1;
+            }
+
+            if (input % 2 == 0) {
+                even++;
+            }
+
+            if (input % 2 == 1) {
+                odd++;
+            }
+        }
+        System.out.println("Sum: " + sum);
+        System.out.println("Numbers: " + numb);
+        System.out.println("Average: " + sum/numb);
+        System.out.println("Even: " + even);
+        System.out.println("Odd " + odd);
     }
 }
