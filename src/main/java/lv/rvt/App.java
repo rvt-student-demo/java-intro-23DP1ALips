7d8295d1f334d087aafa76e0a53c10b864aa22ba
23DP1ALips <166118019+23DP1ALips@users.noreply.github.com>
2024-10-08 09:29:14 +0000

    Advanced astrology

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,29 +7,61 @@ public class App
         for (int i = 1; i <= number; i++) {
             System.out.print('*');
         }
-        System.out.println();
+        
     }
 
     public static void printSquare(int size) {
         for (int i = 0; i < size; i++){
             printStars(size);
+            System.out.println();
         }
     }
 
     public static void printRectangle(int width, int height) {
         for (int i = 1; i <= height; i++) {
             printStars(width);
+            System.out.println();
         }
     }
 
     public static void printTriangle(int size) {
         for (int i = 1; i <= size; i++) {
             printStars(i);
+            System.out.println();
         }
+    }
+
+    public static void printSpaces(int number) {
+        for (int i = 1; i <= number; i++) {
+            System.out.print(' ');
+        }
+    }
+
+    public static void printTriangleTwo(int size) {
+        for (int i = 1; i <= size; i++) {
+            printSpaces(size -i);
+            printStars(i);
+            System.out.println();
+        }
+    }
+
+    public static void printChristmasTree(int height) {
+        int j = 1;
+        for (int i = 1; i <= height; i++) {
+            printSpaces(height - i);
+            printStars(j);
+            j += 2;
+        System.out.println();
+        }
+        printSpaces(height - 2);
+        printStars(3);
+        System.out.println();
+        printSpaces(height - 2);
+        printStars(3);
         System.out.println();
     }
     
     public static void main(String[] args) {
-        printSquare(2);
+        printChristmasTree(10);
     }
 }
