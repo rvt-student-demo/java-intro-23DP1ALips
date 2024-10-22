db6ee03a6a3301ce3f24a7208c0dd9365daa1aef
23DP1ALips <166118019+23DP1ALips@users.noreply.github.com>
2024-10-22 07:15:23 +0000

    Only these numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,24 +3,37 @@ package lv.rvt;
 import java.util.*;
 
 public class App
-
 {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        ArrayList<Integer> intList = new ArrayList<>();
-        while (true) {
-            int input = Integer.valueOf(scanner.nextLine());
-            intList.add(input);
-            if (input == 0) {
-                break;
-            }
+        ArrayList<Integer> numberList = new ArrayList<>();
+
+        numberList.add(Integer.valueOf(scanner.nextLine()));
+
+        int i = Integer.valueOf(scanner.nextLine());
+
+        while(numberList.get(numberList.size()-1) != 1) {
+            numberList.add(Integer.valueOf(scanner.nextLine()));
         }
 
-        int listSum = 0;
-        for (int i = 0; i < intList.size(); i++) {
-            listSum += intList.get(i);
+        System.out.println("From where?");
+        int f = Integer.valueOf(scanner.nextLine());
+
+        while (numberList.size()- 1 < f || f < 0) {
+            f = Integer.valueOf(scanner.nextLine());
         }
-        System.out.println(listSum);
+
+        System.out.println("To where?");
+        int t = Integer.valueOf(scanner.nextLine());
+
+        while (numberList.size() - 1 < t || t < 0) {
+            t = Integer.valueOf(scanner.nextLine());
+        }
+
+        while (f <= t) {
+            System.out.println(numberList.get(f));
+            f++;
+        }
     }
 }
