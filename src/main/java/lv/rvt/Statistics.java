f66e300cc53e2b32c59bbea70cb96d88813f279d
23DP1ALips <166118019+23DP1ALips@users.noreply.github.com>
2024-11-06 14:13:57 +0000

    Sum and Average

--- src/main/java/lv/rvt/Statistics.java
+++ src/main/java/lv/rvt/Statistics.java
@@ -2,6 +2,8 @@ package lv.rvt;
 
 public class Statistics {
     private int count;
+    private int sum;
+    private int num;
 
     public Statistics() {
         
@@ -9,9 +11,20 @@ public class Statistics {
 
     public void addNumber(int number) {
         count++;
+        this.num = number;
+        sum += this.num;
     }
 
     public int getCount() {
         return count;
+    }
+
+    public int sum() {
+        return sum;
+    }
+
+    public double average() {
+        double avg = this.sum / (this.count * 1.00);
+        return avg;
     }
 }
