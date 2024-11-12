4018ad162ffc175738b2d7f2208aebdb1393b01a
23DP1ALips <166118019+23DP1ALips@users.noreply.github.com>
2024-11-12 07:17:11 +0000

    Multiple sums (p4)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,19 +1,31 @@
 package lv.rvt;
-
 import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Statistics statistics = new Statistics();
+        Statistics allNumbers = new Statistics();
+        Statistics evenNumbers = new Statistics();
+        Statistics oddNumbers = new Statistics();
+
         System.out.println("Enter numbers:");
+
         while (true) {
             int number = Integer.valueOf(scanner.nextLine());
             if (number == -1) {
                 break;
             }
-            statistics.addNumber(number);
+            
+            allNumbers.addNumber(number);
+
+            if (number % 2 == 0) {
+                evenNumbers.addNumber(number);
+            } else {
+                oddNumbers.addNumber(number);
+            }
         }
-        System.out.println("Sum: " + statistics.sum());
+
+        System.out.println("Sum: " + allNumbers.sum());
+        System.out.println("Sum of even numbers: " + evenNumbers.sum());
+        System.out.println("Sum of odd numbers: " + oddNumbers.sum());
     }
 }
