0e7db04fe460b39eed9b7a44ddd2050a046c37c3
23DP1ALips <166118019+23DP1ALips@users.noreply.github.com>
2024-11-12 07:26:28 +0000

    Non-negative balance (p3)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,14 +3,13 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        PaymentCard card = new PaymentCard(50);
+        PaymentCard card = new PaymentCard(5);
         System.out.println(card);
         
-        card.eatAffordably();
+        card.eatHeartily();
         System.out.println(card);
 
         card.eatHeartily();
-        card.eatAffordably();
         System.out.println(card);
     }
 }
