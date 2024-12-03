35fcdbcab432c6a720b613c69bbe2209d96bb0e0
23DP1ALips <166118019+23DP1ALips@users.noreply.github.com>
2024-12-03 10:37:49 +0000

    User registration

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,9 +6,21 @@ import java.util.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Person person = new Person("asdfokasdfk", 0, 0, 0);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ievadiet jūsu vārdu: ");
+        String name = scanner.nextLine();
+        System.out.println("Ievadiet jūsu vecumu: ");
+        int age = Integer.valueOf(scanner.nextLine());
+        System.out.println("Ievadiet jūsu svaru: ");
+        int weight = Integer.valueOf(scanner.nextLine());
+        System.out.println("Ievadiet jūsu augstumu: ");
+        int height = Integer.valueOf(scanner.nextLine());
+
+        Person person = new Person(name, age, weight, height);
 
         PersonManager.addPerson(person);
+        System.out.println("Paldies par reģistrāciju!");
     }
     public static void modify(Person person) {
         person.setName("Modified!");
