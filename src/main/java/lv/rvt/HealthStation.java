eea2976b8eae44cec9c13147d12224699f1620b0
23DP1ALips <166118019+23DP1ALips@users.noreply.github.com>
2024-12-10 09:19:25 +0000

    Health Station (part 2)

--- src/main/java/lv/rvt/HealthStation.java
+++ src/main/java/lv/rvt/HealthStation.java
@@ -1,7 +1,9 @@
 package lv.rvt;
-
 public class HealthStation {
     public int weigh(Person person) {
         return person.getWeight();
+    }
+    public void feed(Person person) {
+        person.setWeight(person.getWeight() + 1);
     }
 }
