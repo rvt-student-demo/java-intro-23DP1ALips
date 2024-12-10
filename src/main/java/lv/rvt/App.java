69137a43b4b00aa5fba35c3cea4efde66e335ce1
23DP1ALips <166118019+23DP1ALips@users.noreply.github.com>
2024-12-10 10:30:11 +0000

    Payment Cards (part 2)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,14 +2,17 @@ package lv.rvt;
 
 public class App { 
     public static void main(String[] args) {
-        PaymentCard petesCard = new PaymentCard(10);
-        System.out.println("money " + petesCard.balance());
-        boolean wasSuccessful = petesCard.takeMoney(8);
-        System.out.println("successfully withdrew: " + wasSuccessful);
-        System.out.println("money " + petesCard.balance());
+        PaymentTerminal unicafeExactum = new PaymentTerminal();
 
-        wasSuccessful = petesCard.takeMoney(4);
-        System.out.println("successfully withdrew: " + wasSuccessful);
-        System.out.println("money " + petesCard.balance());
+        double change = unicafeExactum.eatAffordably(10);
+        System.out.println("remaining change " + change);
+
+        change = unicafeExactum.eatAffordably(5);
+        System.out.println("remaining change " + change);
+
+        change = unicafeExactum.eatHeartily(4.3);
+        System.out.println("remaining change " + change);
+
+        System.out.println(unicafeExactum);
     }
 }
