69b8015319f077455997dbb11d9a209cb4b1d9a0
23DP1ALips <166118019+23DP1ALips@users.noreply.github.com>
2024-12-17 07:48:23 +0000

    Payment Cards (part 3)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,18 +1,34 @@
 package lv.rvt; 
+import java.io.BufferedWriter;
+import java.nio.file.StandardOpenOption;
+import java.io.BufferedReader; 
+import java.util.*;
 
+import javax.sound.midi.Soundbank; 
+ 
 public class App { 
     public static void main(String[] args) {
         PaymentTerminal unicafeExactum = new PaymentTerminal();
 
         double change = unicafeExactum.eatAffordably(10);
-        System.out.println("remaining change " + change);
+        System.out.println("remaining change: " + change);
 
-        change = unicafeExactum.eatAffordably(5);
-        System.out.println("remaining change " + change);
+        PaymentCard annesCard = new PaymentCard(7); 
 
-        change = unicafeExactum.eatHeartily(4.3);
-        System.out.println("remaining change " + change);
+        boolean wasSuccessful = unicafeExactum.eatHeartily(annesCard);
+        System.out.println("there was enough money: " + wasSuccessful);
+        wasSuccessful = unicafeExactum.eatHeartily(annesCard);
+        System.out.println("there was enough money: " + wasSuccessful);
+        wasSuccessful = unicafeExactum.eatAffordably(annesCard);
+        System.out.println("there was enough money: " + wasSuccessful);
 
         System.out.println(unicafeExactum);
+    }
+    public static void modify(Person person) {
+        person.setName("Modified!");
+    }
+
+    public static void modify(int value){
+        value += 5;
     }
 }
---
+++ src/main/java/lv/rvt/PaymentCard.java
@@ -0,0 +1,30 @@
+package lv.rvt;
+
+public class PaymentCard {
+    private double balance;
+
+    public PaymentCard(double balance) {
+        this.balance = balance;
+    }
+
+    public double balance() {
+        return this.balance;
+    }
+
+    public String toString() {
+        return "The card has a balance of " + this.balance + " euros"  ;
+    }
+
+    public void addMoney(double increase) {
+        this.balance = this.balance + increase;
+    }
+
+    public boolean takeMoney(double amount) {
+        if (this.balance >= amount) {
+            this.balance -= amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
