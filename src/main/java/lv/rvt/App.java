5c6694b3f7ef1a20fe3d87482bd571872398c6fe
Alberts Lipšāns <166118019+23DP1ALips@users.noreply.github.com>
2025-01-29 13:04:02 +0000

    Box Ex. 2

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -12,11 +12,11 @@ public class App {
 
     public static void main ( String[] args )
     {
-       Box box = new  Box( 2.5, 5.0, 6.0 ) ;
-
-       System.out.println( "Area: "  + box.area() + " volume: " + box.volume() );
-
-       System.out.println(box);
+        Box box = new  Box( 2.5, 5.0, 6.0 ) ;
+    
+        System.out.println( "Area: "  + box.area() + " volume: " + box.volume() );
+        System.out.println("topArea: " + box.getTopArea());
+        System.out.println(box);
 
     }
 }
