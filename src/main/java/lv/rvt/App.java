5469ea5475858ed234d9f16ed725bbbf47bc7ba8
Alberts Lipšāns <166118019+23DP1ALips@users.noreply.github.com>
2025-01-29 13:05:33 +0000

    Box Ex. 3

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,20 +3,20 @@ import java.io.BufferedWriter;
 import java.nio.file.StandardOpenOption;
 import java.io.BufferedReader; 
 import java.util.*;
-
 import javax.sound.midi.Soundbank;
-
 import lv.rvt.connecticut.Box; 
-
+ 
 public class App { 
-
     public static void main ( String[] args )
     {
         Box box = new  Box( 2.5, 5.0, 6.0 ) ;
-    
+
+        Box box1 = new Box(box);
+
         System.out.println( "Area: "  + box.area() + " volume: " + box.volume() );
         System.out.println("topArea: " + box.getTopArea());
         System.out.println(box);
 
+        System.out.println(box1);
     }
 }
--- src/main/java/lv/rvt/connecticut/Box.java
+++ src/main/java/lv/rvt/connecticut/Box.java
@@ -3,8 +3,6 @@ public class Box {
     private double length;
     private double width;
     private double height;
-
-
     public Box(double length, double width, double height){
         this.length = length;
         this.width = width;
@@ -15,40 +13,50 @@ public class Box {
         this.width = side;
         this.height = side;
     }
-    
+
+    public Box( Box oldBox ){
+        this.length = oldBox.length();
+        this.width = oldBox.width();
+        this.height = oldBox.height();
+    }
+
     public double volume(){
         return this.length * this.width * this.height;
     }
-
     public double area(){
         return 2 * (faceArea() + topArea() + sideArea());
     }
-
     public String toString(){
         return "length: " + this.length + " height: " + this.height +" width: " + this.width;
     }
-
     private double faceArea(){
         return this.length * this.height;
     }
-
     private double topArea(){
         return this.length * this.width;
     }
-
     private double sideArea(){
         return this.width * this.height;
     }
-
     public double getFaceArea(){
         return faceArea();
     }
-
     public double getTopArea(){
         return topArea();
     }
-
     public double getSideArea(){
         return sideArea();
+    }
+
+    public double length(){
+        return this.length;
+    }
+
+    public double height(){
+        return this.height;
+    }
+
+    public double width(){
+        return this.width;
     }
 }
