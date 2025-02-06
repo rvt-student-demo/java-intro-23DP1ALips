4538eebb4b9b74889166f8a1c0152fa251bbb9fb
Alberts Lipšāns <166118019+23DP1ALips@users.noreply.github.com>
2025-02-06 13:59:29 +0000

    Warehouseing (p1)

---
+++ src/main/java/lv/rvt/ProductWarehouse.java
@@ -0,0 +1,17 @@
+package lv.rvt;
+
+public class ProductWarehouse extends Warehouse {
+    private String name;
+
+
+    public ProductWarehouse(String productName, double capacity) {
+        super(capacity);
+        this.name = productName;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+
+}
